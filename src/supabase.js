8eda679fc489cc6eebc53cb87baefaaa6fb55fd5
admin01 <paradoxraphael@gmai.com>
2024-09-16 11:06:31 +1000

    update supabase config

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY
 );
 
-export const supabaseUrl = process.env.SUPABASE_URL;
+export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 export default supabase;
