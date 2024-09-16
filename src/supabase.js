bb2bee5f44a33e141d95656d2ea902faf2a4abd6
admin01 <paradoxraphael@gmai.com>
2024-09-16 11:50:09 +1000

    update supabase config

--- src/supabase.js
+++ src/supabase.js
@@ -7,8 +7,8 @@ const SUPABASE_KEY =
   process.env.REACT_APP_SUPABASE_KEY ||
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5amt2bXRxa2Z2YmVudHRqcm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwMTkyNTksImV4cCI6MjAzNjU5NTI1OX0.V7wE07QllKOgophn69YVdZj1QW-W13FZsgV8DEt-k3A";
 
-console.log("Supabase URL:", SUPABASE_URL);
-console.log("Supabase Key:", SUPABASE_KEY);
+console.log("Supabase URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("Supabase Key:", process.env.REACT_APP_SUPABASE_KEY);
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
