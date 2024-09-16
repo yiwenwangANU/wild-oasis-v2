f824b230549b7b49b25a2ecdd9a1321b82166703
admin01 <paradoxraphael@gmai.com>
2024-09-16 11:42:55 +1000

    update supabase config

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL =
-  process?.env?.REACT_APP_SUPABASE_URL || "Missing SUPABASE_URL";
+  process.env.REACT_APP_SUPABASE_URL || "Missing SUPABASE_URL";
 const SUPABASE_KEY =
-  process?.env?.REACT_APP_SUPABASE_KEY || "Missing SUPABASE_KEY";
+  process.env.REACT_APP_SUPABASE_KEY || "Missing SUPABASE_KEY";
 
 console.log("Supabase URL:", SUPABASE_URL);
 console.log("Supabase Key:", SUPABASE_KEY);
